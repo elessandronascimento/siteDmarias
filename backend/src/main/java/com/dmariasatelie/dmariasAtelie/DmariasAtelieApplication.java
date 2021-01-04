b759d288a975a441f127b5f618e5ef7fca60c85b
elessandronascimento <elessandronascimento@gmail.com>
2021-01-04 18:05:22 -0300

    Projeto criated

---
+++ backend/src/main/java/com/dmariasatelie/dmariasAtelie/DmariasAtelieApplication.java
@@ -0,0 +1,13 @@
+package com.dmariasatelie.dmariasAtelie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmariasAtelieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmariasAtelieApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/dmariasatelie/dmariasAtelie/DmariasAtelieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmariasatelie.dmariasAtelie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmariasAtelieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
